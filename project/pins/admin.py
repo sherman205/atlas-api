3b78d6dc567ee4013412c536fc0dacae4282e3b1
Sophia <sherman205@gmail.com>
2020-01-28 21:03:45 -0800

    Add fields in admin view to manage pins

--- project/pins/admin.py
+++ project/pins/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Pin
 
-# Register your models here.
+class PinAdmin(admin.ModelAdmin):
+  list_display = ('user_id', 'date', 'latitude', 'longitude', 'city', 'state', 'country')
+
+admin.site.register(Pin, PinAdmin)
